15c8f97929866b86464ee0dc24b44fb960d10f98
isha vishwakarma <ishavishwakarma29@gmail.com>
2023-06-01 01:37:27 +0530

    initial commit

---
+++ primary.java
@@ -0,0 +1,10 @@
+import  java.awt.Dimension;
+import  java.awt.Image;
+import java.awt.Toolkit;
+import javax.swing.*;
+
+public class primary {
+    int width = (int) Toolkit.getDefaultToolkit().getDefaultToolkit().getScreenSize().getWidth();
+    int height = int width = (int) Toolkit.getDefaultToolkit().getDefaultToolkit().getScreenSize().getWidth();
+
+}
---
+++ src/FlappyFrame.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class FlappyFrame extends JFrame {
+    public FlappyFrame()
+    {
+        setSize(500, 500);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
